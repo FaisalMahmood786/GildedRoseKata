7754214a1ed722fcc36b6505c7f567c001e8a035
Faisal Mahmood <Faisal.Mahmood@evolutionmoney.co.uk>
2024-07-03 10:37:54 +0100

    Additional tests for other items to ensure the refactor didn't break existing functionality

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -35,5 +35,50 @@ namespace GildedRoseTests
 
             Assert.Equal(2, Items[0].Quality);
         }
-    }
+        // Additional tests for other items to ensure the refactor didn't break existing functionality
+        [Fact]
+        public void AgedBrie_IncreasesInQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.Equal(1, Items[0].Quality);
+        }
+
+        [Fact]
+        public void BackstagePasses_IncreaseInQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.Equal(21, Items[0].Quality);
+        }
+
+        [Fact]
+        public void BackstagePasses_QualityDropsToZeroAfterConcert()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Fact]
+        public void Sulfuras_QualityDoesNotChange()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.Equal(80, Items[0].Quality);
+        }
+   }
+
 }
